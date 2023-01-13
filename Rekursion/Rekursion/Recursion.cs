7f89cbfb27d0c70fbffcf7cba3c2d35d5efcefe6
luzzisttoll <91245180+luzzisttoll@users.noreply.github.com>
2023-01-13 13:19:55 +0100

    recursion

---
+++ Rekursion/Rekursion/Recursion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekursion
+{
+    public class Recursion
+    {
+        public int Fakultät_Recursion(int number)
+        {
+            if (number == 0)
+                return 1;
+
+            return number * Fakultät_Recursion(number - 1);
+        }
+
+        public int Fibonacci_Recursion(int number)
+        {
+            if (number < 2)
+                return number;
+            return Fibonacci_Recursion(number - 1) + Fibonacci_Recursion(number - 2);
+        }
+    }
+}
---
+++ Rekursion/Test_Recursive_Functions/Fib_Test.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Rekursion;
+
+namespace Test_Recursive_Functions
+{
+    public class Fib_Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+
+        public void Sum_FibRecursiv_ReturnsCorrectNumber()
+        {
+            Recursion rec = new Recursion();
+            int ret = rec.Fibonacci_Recursion(5);
+            Assert.AreEqual(ret, 5);
+        }
+    }
+}
